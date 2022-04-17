042f0a81218bb80098646313b93734f02530d99c
fender5234 <fender5234@gmail.com>
2022-04-17 22:33:02 +0300

    адаптив 320
    промежуточный коммит
    
    добавляет базовые стили главная,каталог
    
    Добавляет продолжение домашка
    
    Добавляет стилизацию каталога и футер
    
    Добавлет правки хэдэр футер каталог
    
    Добавляет доделка главной и каталога
    
    Домашка мобилка

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,11 @@ import bemValidator from 'gulp-html-bem-validator';
 import browser from 'browser-sync';
 import gulp from 'gulp';
 import lintspaces from 'gulp-lintspaces';
+import eslint from 'gulp-eslint';
 import posthtml from 'gulp-posthtml';
 import rename from 'gulp-rename';
 import less from 'gulp-less';
+import lessSyntax from 'postcss-less';
 import postcss from 'gulp-postcss';
 import autoprefixer from 'autoprefixer';
 import stylelint from 'stylelint';
@@ -18,30 +20,31 @@ const checkLintspaces = () => lintspaces({
 const editorconfigSources = [
   'source/njk/**/*.njk',
   '*.json',
-  '*.js',
   'source/img/**/*.svg'
+];
+const jsSources = [
+  'source/js/**/*.js',
+  '*.js'
 ];
 
 export const buildHTML = () => src('source/njk/pages/**/*.njk')
   .pipe(posthtml())
   .pipe(bemValidator())
   .pipe(rename({ extname: '.html' }))
-  .pipe(dest('source'))
-  .pipe(browser.stream());
+  .pipe(dest('source'));
 
 export const testEditorconfig = () => src(editorconfigSources)
   .pipe(checkLintspaces())
   .pipe(lintspaces.reporter());
 
-export const styles = () => src('source/less/style.less', { sourcemaps: true })
+export const styles = () => src('source/less/*.less', { sourcemaps: true })
   .pipe(less())
   .pipe(postcss([
     autoprefixer()
   ]))
-  .pipe(dest('source/css', { sourcemaps: '.' }))
-  .pipe(browser.stream());
+  .pipe(dest('source/css', { sourcemaps: '.' }));
 
-export const testStyles = () => src('source/less/**/*.less', { sourcemaps: true })
+export const testStyles = () => src('source/less/**/*.less')
   .pipe(checkLintspaces())
   .pipe(lintspaces.reporter())
   .pipe(postcss([
@@ -51,7 +54,17 @@ export const testStyles = () => src('source/less/**/*.less', { sourcemaps: true 
       clearAllMessages: true,
       throwError: false
     })
-  ]));
+  ], {
+    syntax: lessSyntax
+  }));
+
+export const testScripts = () => src(jsSources)
+  .pipe(eslint({
+    fix: false
+  }))
+  .pipe(eslint.format())
+  .pipe(checkLintspaces())
+  .pipe(lintspaces.reporter());
 
 const server = (done) => {
   browser.init({
@@ -65,12 +78,18 @@ const server = (done) => {
   done();
 };
 
+const reload = (done) => {
+  browser.reload();
+  done();
+};
+
 const watcher = () => {
-  watch(editorconfigSources, series(testEditorconfig, buildHTML));
-  watch('source/less/**/*.less', series(testStyles, styles));
+  watch(editorconfigSources, series(testEditorconfig, buildHTML, reload));
+  watch('source/less/**/*.less', series(testStyles, styles, reload));
+  watch(jsSources, series(testScripts, reload));
 };
 
 export const build = parallel(buildHTML, styles);
-export const test = parallel(testEditorconfig, testStyles);
+export const test = parallel(testEditorconfig, testStyles, testScripts);
 
 export default series(test, build, server, watcher);
